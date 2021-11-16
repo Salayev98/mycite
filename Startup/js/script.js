5a1b2ac9bc3a0f1c36ab02bf710e462bce692ab0
Salayev98 <87970921+Salayev98@users.noreply.github.com>
2021-11-16 18:32:54 +0400

    Add files via upload

--- Startup/js/script.js
+++ Startup/js/script.js
@@ -1,26 +1,34 @@
-let items=document.getElementsByClassName("corousel-items");
-let button=document.querySelector("#Previos");
-let button2=document.querySelector("#Next");
-let count=1;
-let count2=items.length-1
-button2.addEventListener("click", function(){
-   for (let i = 0; i < items.length; i++) {
-      items[i].style.display="none"  
-   }
-    if(count>=items.length){
-        count=0
+let items = document.getElementsByClassName("corousel-items");
+let button = document.querySelector("#Previos");
+let button2 = document.querySelector("#Next");
+let count = 1;
+button2.addEventListener("click", function () {
+    button.style.display = "inline"
+    for (let i = 0; i < items.length; i++) {
+        items[i].style.display = "none"
     }
-    items[count].style.display="block";
-    count++; 
+    if (count >= items.length) {
+        count = items.length - 1;
+    }
+    if (count >= items.length - 1) {
+        button2.style.display = "none"
+    }
+    items[count].style.display = "block";
+    count++;
 })
-// button.addEventListener("click",function(){
-    
-//     for (let i = 0; i < items.length; i++) {
-//         items[i].style.display="none"  
-//     }
-//      if(count2<0){
-//         count2=items.length-1
-//     }
-//     items[count2].style.display="block";
-//     count2--;
-// })
+button.addEventListener("click", function () {
+    button2.style.display = "inline"
+    for (let i = 0; i < items.length; i++) {
+        items[i].style.display = "none"
+    }
+
+    count--;
+    if (count <= 0) {
+        count = 1
+    }
+    if (count <= 1) {
+        button.style.display = "none"
+    }
+    items[count - 1].style.display = "block";
+
+})
